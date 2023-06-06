2791b20257368c317c8645f43b89bcf10643398e
Zakaria96 <zakarialavoro@gmail.com>
2023-06-06 10:13:33 +0200

    update

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -10,11 +10,21 @@ export default {
 </script>
 
 <template>
-    <div class="col-6 col-md-4 col-lg-3">
+    <div class="col-4 col-md-3 col-lg-2 card my-2 mx-3">
         <img :src="details.card_images[0].image_url_small" :alt="details.name">
         <h3>{{ details.name }}</h3>
-        <h6>{{ details.archetype }}</h6>
+        <h4>{{ details.archetype }}</h4>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+
+div {
+    background-color: $primary ;
+
+    h3 {
+        color: $white;
+    }
+}
+</style>
